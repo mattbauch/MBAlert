0e18992724c92fab63e42c74059f2e49c749b2fb
Matthias Bauch <dev@matthiasbauch.com>
2013-02-18 16:19:51 +0100

    test alerts with textFields

--- MBAlertDemo/MBAlertViewController.h
+++ MBAlertDemo/MBAlertViewController.h
@@ -13,11 +13,17 @@
 @property (weak, nonatomic) IBOutlet UIButton *uiAlert1Button;
 @property (weak, nonatomic) IBOutlet UIButton *uiAlert2Button;
 @property (weak, nonatomic) IBOutlet UIButton *uiAlert3Button;
+@property (weak, nonatomic) IBOutlet UIButton *uiAlertPlainTextFieldButton;
+@property (weak, nonatomic) IBOutlet UIButton *uiAlertSecretTextFieldButton;
+@property (weak, nonatomic) IBOutlet UIButton *uiLoginTextFieldButton;
 
 @property (weak, nonatomic) IBOutlet UIButton *mbAlertNoButton;
 @property (weak, nonatomic) IBOutlet UIButton *mbAlert1Button;
 @property (weak, nonatomic) IBOutlet UIButton *mbAlert2Button;
 @property (weak, nonatomic) IBOutlet UIButton *mbAlert3Button;
+@property (weak, nonatomic) IBOutlet UIButton *mbAlertPlainTextFieldButton;
+@property (weak, nonatomic) IBOutlet UIButton *mbAlertSecretTextFieldButton;
+@property (weak, nonatomic) IBOutlet UIButton *mbLoginTextFieldButton;
 
 - (IBAction)uiButtonPressed:(UIButton *)sender;
 - (IBAction)mbButtonPressed:(UIButton *)sender;
--- MBAlertDemo/MBAlertViewController.m
+++ MBAlertDemo/MBAlertViewController.m
@@ -32,9 +32,9 @@
     NSString *message = @"This is a message";
     
     UIAlertView *alert = [[UIAlertView alloc] init];
+    alert.title = title;
+    alert.message = message;
     if (sender == self.uiAlertNoButton) {
-        alert.title = title;
-        alert.message = message;
         double delayInSeconds = 2.0;
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
@@ -42,24 +42,30 @@
         });
     }
     else if (sender == self.uiAlert1Button) {
-        alert.title = title;
-        alert.message = message;
         NSInteger index = [alert addButtonWithTitle:@"Cancel"];
         alert.cancelButtonIndex = index;
     }
     else if (sender == self.uiAlert2Button) {
-        alert.title = title;
-        alert.message = message;
         [alert addButtonWithTitle:@"Normal Button"];
         [alert addButtonWithTitle:@"Cancel"];
         alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:@"Cancel" otherButtonTitles:@"Normal Button", nil];
     }
     else if (sender == self.uiAlert3Button) {
-        alert.title = title;
-        alert.message = message;
         [alert addButtonWithTitle:@"Normal Button 1"];
         [alert addButtonWithTitle:@"Normal Button 2"];
         [alert addButtonWithTitle:@"Cancel"];
+    }
+    else if (sender == self.uiAlertPlainTextFieldButton) {
+        [alert addButtonWithTitle:@"Set"];
+        alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    }
+    else if (sender == self.uiAlertSecretTextFieldButton) {
+        [alert addButtonWithTitle:@"Set"];
+        alert.alertViewStyle = UIAlertViewStyleSecureTextInput;
+    }
+    else if (sender == self.uiLoginTextFieldButton) {
+        [alert addButtonWithTitle:@"Set"];
+        alert.alertViewStyle = UIAlertViewStyleLoginAndPasswordInput;
     }
     else {
         abort();
@@ -72,9 +78,9 @@
     NSString *message = @"This is a message";
     
     MBAlertView *alert = [[MBAlertView alloc] init];
+    alert.title = title;
+    alert.message = message;
     if (sender == self.mbAlertNoButton) {
-        alert.title = title;
-        alert.message = message;
         double delayInSeconds = 2.0;
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
@@ -82,16 +88,12 @@
         });
     }
     else if (sender == self.mbAlert1Button) {
-        alert.title = title;
-        alert.message = message;
         NSInteger index = [alert addButtonWithTitle:@"Cancel" action:^{
             NSLog(@"Cancel");
         }];
         alert.cancelButtonIndex = index;
     }
     else if (sender == self.mbAlert2Button) {
-        alert.title = title;
-        alert.message = message;
         [alert addButtonWithTitle:@"Normal Button" action:^{
             NSLog(@"Normal Button");
         }];
@@ -100,8 +102,6 @@
         }];
     }
     else if (sender == self.mbAlert3Button) {
-        alert.title = title;
-        alert.message = message;
         [alert addButtonWithTitle:@"Normal Button 1" action:^{
             NSLog(@"Normal Button 1");
         }];
@@ -112,10 +112,40 @@
             NSLog(@"Cancel");
         }];
     }
+    else if (sender == self.mbAlertPlainTextFieldButton) {
+        __weak MBAlertView *weakAlert = alert;
+        [alert addButtonWithTitle:@"Set" action:^{
+            NSLog(@"Set %@", weakAlert.textField0.text);
+        }];
+        alert.alertStyle = MBAlertViewStylePlainTextInput;
+    }
+    else if (sender == self.mbAlertSecretTextFieldButton) {
+        __weak MBAlertView *weakAlert = alert;
+        [alert addButtonWithTitle:@"Set" action:^{
+            NSLog(@"Set %@", weakAlert.textField0.text);
+        }];
+        alert.alertStyle = MBAlertViewStyleSecureTextInput;
+    }
+    else if (sender == self.mbLoginTextFieldButton) {
+        __weak MBAlertView *weakAlert = alert;
+        [alert addButtonWithTitle:@"Set" action:^{
+            NSLog(@"Set \"%@\" - \"%@\"", weakAlert.textField0.text, weakAlert.textField1.text);
+        }];
+        alert.alertStyle = MBAlertViewStyleLoginAndPasswordInput;
+    }
     else {
         abort();
     }
     [alert show];
 }
 
+- (void)viewDidUnload {
+    [self setUiAlertPlainTextFieldButton:nil];
+    [self setMbAlertPlainTextFieldButton:nil];
+    [self setUiAlertSecretTextFieldButton:nil];
+    [self setMbAlertSecretTextFieldButton:nil];
+    [self setUiLoginTextFieldButton:nil];
+    [self setMbLoginTextFieldButton:nil];
+    [super viewDidUnload];
+}
 @end
