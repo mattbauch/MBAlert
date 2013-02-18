64f0b37059bf2f0e4cf8fffcecc10c2e76d68e75
Matthias Bauch <dev@matthiasbauch.com>
2013-02-18 12:06:52 +0100

    add demo view controller for action sheet

---
+++ MBAlertDemo/MBActionSheetViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBActionSheetViewController.h
+//  MBAlertDemo
+//
+//  Created by Matthias Bauch on 2/18/13.
+//  Copyright (c) 2013 Matthias Bauch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBActionSheetViewController : UIViewController
+
+@end
---
+++ MBAlertDemo/MBActionSheetViewController.m
@@ -0,0 +1,38 @@
+//
+//  MBActionSheetViewController.m
+//  MBAlertDemo
+//
+//  Created by Matthias Bauch on 2/18/13.
+//  Copyright (c) 2013 Matthias Bauch. All rights reserved.
+//
+
+#import "MBActionSheetViewController.h"
+
+@interface MBActionSheetViewController ()
+
+@end
+
+@implementation MBActionSheetViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
