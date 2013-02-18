a55fe97b2b1c4aa822d12bd5558c87d7bd21667e
Matthias Bauch <dev@matthiasbauch.com>
2013-02-18 12:12:40 +0100

    Add MBAlertView class (UIView)

---
+++ MBAlertView.h
@@ -0,0 +1,13 @@
+//
+//  MBAlertView.h
+//  MBAlertDemo
+//
+//  Created by Matthias Bauch on 2/18/13.
+//  Copyright (c) 2013 Matthias Bauch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBAlertView : UIView
+
+@end
---
+++ MBAlertView.m
@@ -0,0 +1,31 @@
+//
+//  MBAlertView.m
+//  MBAlertDemo
+//
+//  Created by Matthias Bauch on 2/18/13.
+//  Copyright (c) 2013 Matthias Bauch. All rights reserved.
+//
+
+#import "MBAlertView.h"
+
+@implementation MBAlertView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
