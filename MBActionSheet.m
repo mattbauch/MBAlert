e9549d495fd293db1e8e19016d4e10471e904837
Matthias Bauch <dev@matthiasbauch.com>
2013-02-18 12:12:25 +0100

    Add MBActionSheet class (UIView)

---
+++ MBActionSheet.m
@@ -0,0 +1,31 @@
+//
+//  MBActionSheet.m
+//  MBAlertDemo
+//
+//  Created by Matthias Bauch on 2/18/13.
+//  Copyright (c) 2013 Matthias Bauch. All rights reserved.
+//
+
+#import "MBActionSheet.h"
+
+@implementation MBActionSheet
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
